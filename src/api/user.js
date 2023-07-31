8ff9ab0e50b7fbeb1fc5df1e1fbf633c2f3afa9c
peris <964480899@qq.com>
2023-07-31 23:01:44 +0800

    home页面的channels模块完成（包括未登录和已登录的channels新增和删除）

--- src/api/user.js
+++ src/api/user.js
@@ -41,3 +41,20 @@ export const getUserChannelById = (params) => {
     params
   })
 }
+
+// set user channel for id of channel
+export const setUserChannelById = (data) => {
+  return axios({
+    method: 'PATCH',
+    url: '/v1_0/user/channels',
+    data
+  })
+}
+
+// delete user channel for id of channel
+export const deleteUserChannelById = (channelId) => {
+  return axios({
+    method: 'DELETE',
+    url: `/v1_0/user/channels/${channelId}`
+  })
+}
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -17,7 +17,13 @@
       </van-nav-bar>
     </div>
     <div class="main">
-      <van-tabs @click="fn" :border="true" class="ps-van-tabs" line-width="15">
+      <van-tabs
+        v-model="tabsActive"
+        @click="fn"
+        :border="true"
+        class="ps-van-tabs"
+        line-width="15"
+      >
         <van-tab
           :key="channel.id"
           v-for="channel in userChannels"
@@ -26,79 +32,150 @@
         >
           <channel-list :channel="channel" />
         </van-tab>
+        <div slot="nav-right" style="width: 40px; flex-shrink: 0"></div>
       </van-tabs>
+      <div class="menuBtn">
+        <van-icon name="wap-nav" @click="showPopPup" />
+      </div>
     </div>
+
+    <!--  Popup -->
+    <van-popup
+      closeable
+      close-icon-position="top-left"
+      round
+      :overlay="false"
+      v-model="popPup"
+      position="bottom"
+      get-container="body"
+      class="popPup"
+    >
+      <edit-channel
+        :userChannels="userChannels"
+        :recommendChannels="recommendChannels"
+        :tabsActive="tabsActive"
+        @deleteChannel="deleteChannel"
+        @selectChannel="selectChannel"
+        @addChannel="addChannel"
+      ></edit-channel>
+    </van-popup>
   </div>
 </template>
 
 <script>
-// import { mapState } from 'vuex'
 import channelList from '@/views/home/components/channel-list'
-import { getUserChannels, getUserChannelById } from '@/api/user'
+import editChannel from '@/views/home/components/edit-channel'
+import { mapState } from 'vuex'
+import {
+  getUserChannels,
+  setUserChannelById,
+  deleteUserChannelById
+} from '@/api/user'
+import { getAllChannels } from '@/api/channel'
+import { getItem, setItem } from '@/utils/handleStorage'
+import { USER_CHANNEL_KEY } from '@/config/user'
 export default {
   name: 'HomeView',
   data() {
     return {
       userChannels: [],
+      allChannels: [],
       list: [],
       loading: false,
       finished: false,
       refreshing: false,
-      defaultChannel: 0
+      defaultChannel: 0,
+      popPup: true,
+      tabsActive: 0
     }
   },
   components: {
-    channelList
+    channelList,
+    editChannel
   },
   computed: {
-    // ...mapState(['user'])
+    ...mapState(['user']),
+    recommendChannels() {
+      return this.allChannels.filter((channel) => {
+        return !this.userChannels.find((userChannel) => {
+          return userChannel.id === channel.id
+        })
+      })
+    }
   },
   methods: {
     fn(val, a) {
       console.log(val, a)
     },
-    async onLoad() {
-      try {
-        const params = {
-          channel_id: this.defaultChannel,
-          timestamp: +new Date()
-        }
-        const { data } = await getUserChannelById(params)
-        if (this.refreshing) {
-          this.list = []
-          this.refreshing = false
-        }
-        this.list = [...this.list, ...data.data.results]
-        this.loading = false
-        if (data.data.results.length < 10 || data.data.results.length === 0) {
-          this.finished = true
-        }
-      } catch (err) {
-        //
-        console.log(err)
+    async deleteChannel(id) {
+      const index = this.userChannels.findIndex(
+        (channel) => channel.id === Number(id)
+      )
+      this.userChannels.splice(index, 1)
+      if (!this.user) {
+        setItem(USER_CHANNEL_KEY, this.userChannels)
+      } else {
+        await deleteUserChannelById(Number(id))
       }
     },
-    onRefresh() {
-      // 清空列表数据
-
-      this.finished = false
-      this.onLoad()
+    selectChannel(index) {
+      if (!index) return
+      this.tabsActive = Number(index)
+      this.popPup = false
+    },
+    async addChannel(id) {
+      const index = this.allChannels.findIndex(
+        (channel) => channel.id === Number(id)
+      )
+      const deepVal = this.$_.cloneDeep(this.allChannels[index])
+      this.userChannels.push(deepVal)
+      if (!this.user) {
+        // localStorage save
+        setItem(USER_CHANNEL_KEY, this.userChannels)
+      } else {
+        // sql save
+        await setUserChannelById({
+          channels: [
+            {
+              id: Number(id),
+              seq: this.userChannels.length
+            }
+          ]
+        })
+      }
+    },
+    showPopPup() {
+      this.popPup = true
     },
     async initUserChannels() {
       try {
         const { data } = await getUserChannels()
         this.userChannels = data.data.channels
-        console.log(this.userChannels)
+        if (!this.user) {
+          setItem(USER_CHANNEL_KEY, this.userChannels)
+        }
       } catch (err) {
         console.log(err)
       }
+    },
+    async initAllChannels() {
+      const { data } = await getAllChannels()
+      this.allChannels = data.data.channels
     }
   },
   mounted() {
-    // this.onLoad()
-    this.initUserChannels()
-    // if (this.user !== null) {
-    // }
+    // init all channels
+    this.initAllChannels()
+    if (this.user) {
+      // init user channels
+      this.initUserChannels()
+    } else {
+      if (getItem(USER_CHANNEL_KEY)) {
+        this.userChannels = getItem(USER_CHANNEL_KEY)
+      } else {
+        this.initUserChannels()
+      }
+    }
   }
 }
 </script>
@@ -125,6 +202,21 @@ export default {
     }
   }
   .main {
+    position: relative;
+    .menuBtn {
+      position: absolute;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      width: 40px;
+      height: 40px;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 0;
+      font-size: 30px;
+      z-index: 99;
+      background-color: rgba(255, 255, 255, 0.9);
+    }
     .ps-van-tabs {
       ::v-deep .van-tabs__wrap {
         border-bottom: 1px solid #f0f0f1;
